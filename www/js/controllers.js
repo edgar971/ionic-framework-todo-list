f160cdf7cde82a0c8a277263c082030bdd5e6a10
Edgar Pino <me@edgarpino.com>
2015-01-02 00:18:12 -0600

    Added Item menu
    added the ability to delete and reorder list items.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,6 +8,31 @@ angular.module('starter.controllers', [])
   		});
   		
   		
+  		
+  		$scope.data = {
+	  		showDelete: false,
+	  		showOrder: false
+  		}
+  		
+  		$scope.triggerDelete = function() {
+	  		$scope.data.showDelete = !$scope.data.showDelete;
+	  		$scope.data.showOrder = false;
+	  		console.log($scope.data);
+  		}
+  		
+  		$scope.triggerReOrder = function() {
+	  		$scope.data.showOrder = !$scope.data.showOrder;
+	  		$scope.data.showDelete = false;
+	  		console.log($scope.data);
+  		}
+  		
+  		$scope.moveItem = function(item,from, to) {
+	  		console.log(arguments);
+	  		$scope.TodoListItems.splice(from, 1);
+	  		$scope.TodoListItems.splice(to, 0, item);
+  		}
+  		
+  		
   		$scope.openItemModal = function() {
 	  		$scope.modal.show();
   		}
@@ -15,6 +40,10 @@ angular.module('starter.controllers', [])
   		$scope.closeItemModal = function() {
 	  		$scope.modal.hide();
 	  		
+  		}
+  		
+  		$scope.deleteTodo = function(item) {
+	  		$scope.TodoListItems.splice($scope.TodoListItems.indexOf(item), 1);
   		}
   		
   		//clean up modal
@@ -34,22 +63,27 @@ angular.module('starter.controllers', [])
 		}
 		$scope.TodoListItems = [
 			{
+				id: 3,
 				task: 'Scipio vero et Rutilius multo etiam magis.',
 				done: 'false'
 			}, 
 			{
+				id: 1,
 				task: 'Quamquam philosophiae quidem vituperatoribus satis responsum est eo libro.',
 				done: 'false'
 			},
 			{
+				id: 9,
 				task: 'Terentii quam utramque Menandri legam?',
 				done: 'false'
 			},
 			{
+				id: 6,
 				task: 'Maecenas accumsan nec libero ut posuere.',
 				done: 'false'
 			},
 			{
+				id: 5,
 				task: 'Consentinis et Siculis scribere.',
 				done: 'false'
 			},
